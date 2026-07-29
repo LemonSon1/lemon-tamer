fc6296df905954b7bb53229f4221be69e26bbe21
LemonSon1 <102466644+LemonSon1@users.noreply.github.com>
2026-07-29 19:20:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,44 +3,51 @@ const bubbleContainer = document.getElementById("bubbles");
 function createBubble(){
 
     const bubble = document.createElement("div");
+
     bubble.classList.add("bubble");
 
-    const size = Math.random()*8+3;
+    // Bigger bubbles
+    const size = Math.random() * 18 + 8;
 
-    bubble.style.width = size+"px";
-    bubble.style.height = size+"px";
+    bubble.style.width = size + "px";
+    bubble.style.height = size + "px";
 
-    bubble.style.left = Math.random()*100+"vw";
+    bubble.style.left = Math.random() * 100 + "vw";
 
     bubble.style.animationDuration =
-        (Math.random()*4+3)+"s";
+        (Math.random() * 4 + 4) + "s";
 
-    bubble.style.opacity = Math.random();
+    bubble.style.opacity = Math.random() * .5 + .5;
 
     bubbleContainer.appendChild(bubble);
 
     setTimeout(()=>{
         bubble.remove();
-    },7000);
+    },8000);
 
 }
 
-const bubbleInterval = setInterval(createBubble,120);
+const bubbleInterval = setInterval(createBubble,90);
 
+// End loading
 setTimeout(()=>{
 
     clearInterval(bubbleInterval);
 
-    const loader=document.getElementById("loader");
-    const content=document.getElementById("content");
+    const loader = document.getElementById("loader");
+    const content = document.getElementById("content");
 
-    loader.style.transition="opacity .8s";
-    loader.style.opacity="0";
+    loader.style.transition = "opacity .9s ease";
+    loader.style.opacity = "0";
 
     setTimeout(()=>{
+
         loader.remove();
-        document.body.style.overflow="auto";
-        content.style.opacity="1";
-    },800);
+
+        document.body.style.overflow = "auto";
+
+        content.style.opacity = "1";
+
+    },900);
 
 },4000);
